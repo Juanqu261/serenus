c026b3cb437952be692f0dfdad3e08625f10be63
Alejandro Feria Gonzalez <aferiag@unal.edu.co>
2025-04-26 20:14:23 -0500

    tailwind

---
+++ Desarrollo/frontend/src/administrador/administrador.tsx
@@ -0,0 +1,12 @@
+
+
+function Admin() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Admin Page</h1>
+      <p className="text-lg">Welcome to the admin page!</p>
+    </div>
+  )
+}
+
+export default Admin
---
+++ Desarrollo/frontend/src/chatbotui/Chatbot.tsx
@@ -0,0 +1,11 @@
+
+
+function Chatbot() {
+  return (
+    <div>
+      <h1 className="text-red-500">Chatbot</h1>
+    </div>
+  )
+}
+
+export default Chatbot
